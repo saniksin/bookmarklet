942ca148bd7bcd3fed170f0b7dc49e91af6c212c
Oleksandr Makedon <rbkrutoi@gmail.com>
2023-04-26 00:15:01 +0200

    alpha

--- account/views.py
+++ account/views.py
@@ -1,3 +1,27 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
 # Create your views here.
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request,
+                                username=cd['username'],
+                                password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Авторизация успешна')
+                else:
+                    return HttpResponse('Аккаунт заблокирован')
+            else:
+                return HttpResponse('Ошибка авторизации')
+    else:
+        form = LoginForm()
+    
+    return render(request, 'account/login.html', {'form':form})
